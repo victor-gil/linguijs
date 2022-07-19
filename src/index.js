e272749114eab0c51e6137ce5c90ab8021b1b037
Víctor Gil <victor.gil@geniac.com>
2022-07-19 12:04:08 +0200

    :construction: [WIP] work in progress

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//_index.js_
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello!!</h1>
+      <h2>Welcome to your First React App..!</h2>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
