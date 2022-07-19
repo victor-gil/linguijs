0f7cccc7403fb08693bfa1ad24ad7e36b0434129
Víctor Gil <victor.gil@geniac.com>
2022-07-19 12:28:58 +0200

    :construction: [WIP] work in progress

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-//_index.js_
+// src/index.js
+import React from 'react'
+import { render } from 'react-dom'
 
-import React from "react";
-import ReactDOM from "react-dom";
+import { i18n } from '@lingui/core'
+import { I18nProvider } from '@lingui/react'
+import { messages } from './locales/en/messages'
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello!!</h1>
-      <h2>Welcome to your First React App..!</h2>
-    </div>
-  );
-};
+import { Trans } from '@lingui/macro'
+i18n.load('en', messages)
+i18n.activate('en')
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const App = () => (
+  <I18nProvider i18n={i18n}>
+    <h1><Trans>Hello</Trans></h1>
+  </I18nProvider>
+)
+
+render(<App />, document.getElementById('root'))
